484f25dc35f5e91b2c63cccfb304ac1cba7ee599
MrRezoo <rezam578@gmail.com>
2021-05-21 21:00:23 +0430

    DeleteView UpdateView CreateView

--- first/urls.py
+++ first/urls.py
@@ -4,11 +4,13 @@ from django.views.generic import TemplateView
 from first import views
 
 app_name = 'first'
-
 urlpatterns = [
     # path('', views.home, name='home')
     # path('', TemplateView.as_view(template_name='first/home.html'), name='home')
     path('', views.Home.as_view(), name='home'),
     # path('<int:pk>/', views.DetailTodo.as_view(), name='detail_todo'),
-    path('<slug:myslug>/', views.DetailTodo.as_view(), name='detail_todo')
+    path('create/', views.TodoCreate.as_view(), name='create_todo'),
+    path('<slug:myslug>/', views.DetailTodo.as_view(), name='detail_todo'),
+    path('delete/<int:pk>/', views.DeleteTodo.as_view(), name='delete_todo'),
+    path('update/<int:pk>/', views.UpdateTodo.as_view(), name='update_todo'),
 ]
--- first/views.py
+++ first/views.py
@@ -1,14 +1,18 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.text import slugify
 from django.views import View
 
 # Create your views here.
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, CreateView, DeleteView, UpdateView
 
+from first.forms import TodoCreateForm
 from first.models import Todo
 
 
 class Home(ListView):
-    # template_name = 'first/home.html'  # first/todo_list.html
+    # template_name = 'first/home.html'  # default => first/todo_list.html
     # queryset = Todo.objects.all() # object_list
     # model = Todo
     context_object_name = 'todos'
@@ -44,3 +48,54 @@ class DetailTodo(DetailView):  # AppName/ModelName_detail.html
             return Todo.objects.filter(slug=self.kwargs['myslug'])
         else:
             return Todo.objects.none()
+
+
+# class TodoCreate(FormView):
+#     template_name = 'first/todo_create.html'
+#     form_class = TodoCreateForm
+#     success_url = reverse_lazy('first:home')
+#     # success_url = '/create/'
+#
+#     def form_valid(self, form):
+#         self.create_todo(form.cleaned_data)
+#         return super().form_valid(form)
+#
+#     def create_todo(self, data):
+#         todo = Todo(title=data['title'], slug=slugify(data['title']))
+#         todo.save()
+#         messages.success(self.request, 'your todo added', 'success')
+
+
+class TodoCreate(CreateView):
+    model = Todo
+    fields = ('title',)
+    template_name = 'first/todo_create.html'
+    success_url = reverse_lazy('first:home')
+
+    def form_valid(self, form):
+        todo = form.save(commit=False)
+        todo.slug = slugify(form.cleaned_data['title'])
+        todo.save()
+        messages.success(self.request, 'your todo added', 'success')
+        return super().form_valid(form)
+
+
+class DeleteTodo(DeleteView):
+    model = Todo
+    template_name = 'first/todo_delete.html'
+    success_url = reverse_lazy('first:home')
+
+
+class UpdateTodo(UpdateView):
+    model = Todo
+    fields = ('title',)
+    template_name = 'first/todo_update.html'
+    success_url = reverse_lazy('first:home')
+
+    def form_valid(self, form):
+        todo = form.save(commit=False)
+        todo.slug = slugify(form.cleaned_data['title'])
+        todo.save()
+        return super().form_valid(form)
+
+
